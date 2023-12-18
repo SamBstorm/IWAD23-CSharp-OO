ba1dadacad0082932a84f01bfbed318c205e3a01
Samuel Legrain <samuel.legrain@bstorm.be>
2023-12-18 16:28:14 +0100

    Ajout Correction Exo-Login Part01

---
+++ Exo-Login/Login.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo_Login
+{
+    internal class Login
+    {
+        private string _email;
+        private string _password;
+
+        public event EventHandler LoginOkEvent = null;
+        public event EventHandler LoginNotOkEvent = null;
+
+        public Login(string email, string password)
+        {
+            _email = email;
+            _password = password;
+        }
+
+        public void CheckLogin(string email, string password)
+        {
+            if (_email == email && _password == password) LoginOkEvent?.Invoke(this, EventArgs.Empty);
+            else LoginNotOkEvent?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ Exo-Login/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Exo_Login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Login login = new Login("samuel.legrain@bstorm.be", "Test1234=");
+            login.LoginOkEvent += NotifySuccess;
+            login.LoginNotOkEvent += NotifyFailure;
+            //login.LoginNotOkEvent += delegate (object sender, EventArgs e) {
+            //    Console.BackgroundColor = ConsoleColor.Yellow;
+            //    Console.ForegroundColor = ConsoleColor.Black;
+            //    Console.WriteLine("L'email et/ou le mot de passe ne correspond pas...");
+            //    Console.ResetColor();
+            //};
+
+            login.CheckLogin("sam@fait.rire", "toto");
+            login.CheckLogin("samuel.legrain@bstorm.be", "Test1234=");
+
+        }
+
+        private static void NotifySuccess(object sender, EventArgs e) {
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Bienvenu!");
+            Console.ResetColor();
+        }
+
+        private static void NotifyFailure (object sender, EventArgs e) {
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("L'email et/ou le mot de passe ne correspond pas...");
+            Console.ResetColor();
+        }
+    }
+}
