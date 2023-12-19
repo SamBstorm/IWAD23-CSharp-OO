e1a92a3bd243f05d9113a98e103322bd6881a0cf
Samuel Legrain <samuel.legrain@bstorm.be>
2023-12-19 16:49:10 +0100

    Ajout Démonstration Tuple

---
+++ Demo-Tuple/Personne.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Tuple
+{
+    internal class Personne
+    {
+        public string Nom { get; private set; }
+        public string Prenom { get; private set; }
+
+        public Personne(string nom, string prenom)
+        {
+            Nom = nom;
+            Prenom = prenom;
+        }
+
+        public void Deconstruct(out string nom, out string prenom)
+        {
+            nom = this.Nom;
+            prenom = this.Prenom;
+        }
+    }
+}
---
+++ Demo-Tuple/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Demo_Tuple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Personne p1 = new Personne("Bouharat","Najoua");
+
+            var tupleP1 = (Nom : "Rottin", Prenom : "Elena");
+
+            (string Nom, string Prenom) tupleP2 = ("Chyabri", "Hanane");
+
+            (string Nom, string Prenom) = ("Mucowintore", "Dorine");
+
+            Console.WriteLine($"{tupleP1.Nom} {tupleP1.Prenom}");
+            Console.WriteLine($"{tupleP2.Nom} {tupleP2.Prenom}");
+
+            Console.WriteLine($"{Nom} {Prenom}");
+
+            (string nomDeFamille, string prenomP1) = p1;
+
+            Console.WriteLine($"{nomDeFamille} {prenomP1}");
+        }
+    }
+}
